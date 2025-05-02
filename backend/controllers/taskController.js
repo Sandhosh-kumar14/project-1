c73bcf8f2a12464f8ceb9ac3ace396088e46ec45
Sandhosh-kumar14 <k.sandhoshkumar0114@gmail.com>
2025-05-03 02:38:16 +0530

    Update taskController.js

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,7 +1,5 @@
 import Task from '../models/Task.js';
 import User from '../models/User.js';
-import sendEmail from '../utils/emailService.js';
-import { sendTaskUpdateEmail } from '../utils/emailService.js';
 
 
 // Get all tasks
